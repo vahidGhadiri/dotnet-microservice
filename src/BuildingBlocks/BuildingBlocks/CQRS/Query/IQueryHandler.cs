728f9d0faeb94b1d5a5c1430ca73886b6f0b943a
vahidGhadiri <whydrf@gmail.com>
2026-08-15 09:21:31 +0330

    chore: add IQueryHandler interface as an abstraction over MediatR

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/Query/IQueryHandler.cs
@@ -0,0 +1,8 @@
+using BuildingBlocks.CQRS.Query;
+using MediatR;
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
