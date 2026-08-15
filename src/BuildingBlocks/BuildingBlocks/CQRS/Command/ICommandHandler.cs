52782989d2952059f275ccf4e7a2c5ea38563064
vahidGhadiri <whydrf@gmail.com>
2026-08-15 09:21:19 +0330

    chore: add ICommandHandler interface as an abstraction over MediatR

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/Command/ICommandHandler.cs
@@ -0,0 +1,16 @@
+namespace BuildingBlocks.CQRS.Command;
+
+using MediatR;
+
+public interface ICommandHandler<in TCommand>
+    : IRequestHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
