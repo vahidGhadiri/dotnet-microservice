67f27c602c9d6fc9b6cc888f800fbccd38368d85
vahidGhadiri <whydrf@gmail.com>
2026-08-16 13:00:00 +0330

    chore: add mediatR as a service to prgram.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
+builder.Services.AddMediatR(configuration =>
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly)
+);
 
 
 var app = builder.Build();
