f53d4a2d56111519228c9b630bc5eb2a2fa6e733
vahidGhadiri <whydrf@gmail.com>
2026-08-31 10:25:30 +0330

    chore: add execptions to global using

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,6 +1,6 @@
 global using BuildingBlocks.CQRS.Command;
-
 global using BuildingBlocks.CQRS.Query;
+global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
 global using MediatR;
 global using Mapster;
